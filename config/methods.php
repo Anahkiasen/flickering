cb36af86db13e84e837e1b36b438d1513fd1add7
Anahkiasen <ehtnam6@gmail.com>
2013-02-28 01:46:04 +0100

    Add collectionsGetTree

--- config/methods.php
+++ config/methods.php
@@ -2,4 +2,5 @@
   'photosetsGetList'   => array('user_id', 'page', 'per_page'),
   'photosetsGetPhotos' => array('photoset_id', 'extras', 'privacy_filter', 'per_page', 'page', 'media'),
   'peopleGetPhotos'    => array('user_id', 'safe_search', 'min_upload_date', 'max_upload_date', 'min_taken_date', 'max_taken_date', 'content_type', 'privacy_filter', 'extras', 'per_page', 'page'),
+  'collectionsGetTree' => array('collection_id', 'user_id'),
 );
